fc7158dc61de645d1115c3b7c96cc0cfc4a90b42
geekarist <christophe.pele@gmail.com>
2016-04-01 08:19:55 +0200

    Display picked place address

--- app/src/main/java/com/github/geekarist/whereishome/ShowCommutingTimeActivity.java
+++ app/src/main/java/com/github/geekarist/whereishome/ShowCommutingTimeActivity.java
@@ -4,24 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import butterknife.Bind;
+import butterknife.OnClick;
+
 public class ShowCommutingTimeActivity extends AppCompatActivity {
 
-    private static final int PLACE_PICKER_REQUEST = 1;
+    private static final int REQUEST_PLACE_PICKER = 1;
+
+    @Bind(R.id.list_commutes)
+    RecyclerView mCommuteListView;
 
     private GoogleApiClient mGoogleApiClient;
 
@@ -30,12 +37,10 @@ public class ShowCommutingTimeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_commuting_time);
 
-        RecyclerView commuteListView = (RecyclerView) findViewById(R.id.list_commutes);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        assert commuteListView != null;
-        commuteListView.setLayoutManager(layoutManager);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mCommuteListView.setLayoutManager(layoutManager);
         CommuteListAdapter adapter = new CommuteListAdapter();
-        commuteListView.setAdapter(adapter);
+        mCommuteListView.setAdapter(adapter);
 
         mGoogleApiClient = new GoogleApiClient
                 .Builder(this)
@@ -44,25 +49,30 @@ public class ShowCommutingTimeActivity extends AppCompatActivity {
                 .enableAutoManage(this, null)
                 .build();
         mGoogleApiClient.connect();
+    }
 
-        Button buttonAdd = (Button) findViewById(R.id.button_add_place);
-        assert buttonAdd != null;
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    Intent intent = new PlacePicker.IntentBuilder().build(ShowCommutingTimeActivity.this);
-                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
-                } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
-                    StringWriter msg = new StringWriter();
-                    PrintWriter msgPrint = new PrintWriter(msg);
-                    e.printStackTrace(msgPrint);
-                    new AlertDialog.Builder(ShowCommutingTimeActivity.this)
-                            .setTitle("Google Play Services Error")
-                            .setMessage(msg.toString()).create().show();
-                }
+    @OnClick(R.id.button_add_place)
+    public void onClickButtonAddPlace(View v) {
+        try {
+            Intent intent = new PlacePicker.IntentBuilder().build(ShowCommutingTimeActivity.this);
+            startActivityForResult(intent, REQUEST_PLACE_PICKER);
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+            StringWriter msg = new StringWriter();
+            PrintWriter msgPrint = new PrintWriter(msg);
+            e.printStackTrace(msgPrint);
+            new AlertDialog.Builder(ShowCommutingTimeActivity.this)
+                    .setTitle("Google Play Services Error")
+                    .setMessage(msg.toString()).create().show();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PLACE_PICKER) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(this, data);
+                Toast.makeText(this, place.getAddress(), Toast.LENGTH_LONG).show();
             }
-        });
+        }
     }
 
     @Override
