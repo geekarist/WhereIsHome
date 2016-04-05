9886b8f253155072c14d5bd8bad628149a7b4cb9
geekarist <christophe.pele@gmail.com>
2016-04-05 08:11:19 +0200

    Reformat adapter code

--- app/src/main/java/com/github/geekarist/whereishome/CommuteListAdapter.java
+++ app/src/main/java/com/github/geekarist/whereishome/CommuteListAdapter.java
@@ -1,16 +1,13 @@
 package com.github.geekarist.whereishome;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.google.android.gms.location.places.Place;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class CommuteListAdapter extends RecyclerView.Adapter<CommuteViewHolder> {
