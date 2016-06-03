91f0afb4a46633a2c234ab87852454a81ff160ab
geekarist <christophe.pele@gmail.com>
2016-06-03 08:19:57 +0200

    Initialize uiautomator test (WIP)

--- app/src/androidTest/java/com/github/geekarist/whereishome/ApplicationTest.java
+++ app/src/androidTest/java/com/github/geekarist/whereishome/ApplicationTest.java
@@ -1,6 +1,11 @@
 package com.github.geekarist.whereishome;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +13,21 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class ApplicationTest {
 
+    private UiDevice mDevice;
+
     @Test
     public void shouldWork() {
 
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        mDevice.pressHome();
+        String name = getLauncherPackageName();
+    }
+
+    private String getLauncherPackageName() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        PackageManager packageManager = InstrumentationRegistry.getContext().getPackageManager();
+        ResolveInfo resolveInfo = packageManager.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return resolveInfo.activityInfo.packageName;
     }
 }
